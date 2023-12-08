ef4a3fe576ec4eecd0c2eafc2b3c2f032e7a0fb4
Ashley Wessel <amerino.dance@gmail.com>
2023-12-07 20:41:09 -0600

    routing to next page successfully with test button

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { Button } from "@mui/material";
+import { useHistory } from 'react-router-dom';
 
 function MovieList() {
-
+  let history= useHistory();
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
+
+
+  const clickedMovie = (event) => {
+      history.push("/details/:id")
+
+  }
 
   return (
     <main>
@@ -23,6 +31,7 @@ function MovieList() {
             </div>
           );
         })}
+        <Button onClick={clickedMovie}>Next Page</Button>
       </section>
     </main>
 
