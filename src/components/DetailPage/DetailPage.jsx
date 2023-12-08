df6401b85abe3bcdfdf25e5465b45b30432968c5
Ashley Wessel <amerino.dance@gmail.com>
2023-12-07 22:57:28 -0600

    in process of setting up showClickedMovie function on DetailPage

--- src/components/DetailPage/DetailPage.jsx
+++ src/components/DetailPage/DetailPage.jsx
@@ -1,14 +1,38 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router';
+import axios from 'axios';
+//import { Button } from '@mui/material';
+//import Card from '@mui/material/Card';
+
 
 
 function DetailPage() {
-   
+   const dispatch = useDispatch();
+   const history = useHistory();
+   const params = useParams();
+
+   const [selectedMovie, setSelectedMovie] = useState({});
+
+   const showClickedMovie = () => {
+    axios.get(`/api/movie/${params.id}`).then((result) => {
+        console.log(result.data);
+        let theMovie = {...result.data[0]};
+        setSelectedMovie(theMovie);
+    }).catch((error) => {
+        console.error(`Error in GET 'api/movie/{params.id}`, error);
+        alert('Something went wrong!')
+    })
+}
+
 
     return (
         <>
-        <h1>Hello!</h1>
+        <p>Hello!</p>
         
         </>
     );
-};
+}
 
 export default DetailPage;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
-import { Button } from "@mui/material";
+//import { Button } from "@mui/material";
 import { useHistory } from 'react-router-dom';
 
 function MovieList() {
@@ -15,7 +15,7 @@ function MovieList() {
 
 
   const clickedMovie = (event) => {
-      history.push("/details/:id")
+      history.push("/detailpage/:id")
 
   }
 
@@ -27,11 +27,10 @@ function MovieList() {
           return (
             <div data-testid='movieItem' key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <img src={movie.poster} alt={movie.title} onClick={clickedMovie}/>
             </div>
           );
         })}
-        <Button onClick={clickedMovie}>Next Page</Button>
       </section>
     </main>
 
